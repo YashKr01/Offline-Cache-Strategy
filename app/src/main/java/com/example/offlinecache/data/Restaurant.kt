3b97d9bbfd317f35e57f52944732d8ceb958c921
yash <yashkumar201301@gmail.com>
2021-03-26 22:39:54 +0530

    initial commit

---
+++ app/src/main/java/com/example/offlinecache/data/Restaurant.kt
@@ -0,0 +1,7 @@
+package com.example.offlinecache.data
+
+
+data class Restaurant(
+    private val name: String, private val type: String, private val logo: String
+    , private val address: String
+)
